7b2bcd750bc2884a954574348f60180f80ff38c4
nikhilmohanty-creator <nm695@exeter.ac.uk>
2025-03-13 18:55:27 +0000

    Update useCase14CreateProductInGroup.py

--- Python/UseCase/useCase14CreateProductInGroup.py
+++ Python/UseCase/useCase14CreateProductInGroup.py
@@ -2,42 +2,29 @@ import sys
 import os
 from datetime import datetime
 from bson.objectid import ObjectId
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../Library')))
 from databaseConnection import getMongoConnection
 
-def createProductInGroup(productGroupId, productData):
-    """
-    Add a new product to the product group document.
-    :param productGroupId: The _id of the product group document.
-    :param productData: A dictionary with product details (must include pkProduct, productName, productDescription, productImagePath,
-                        productPrice, stock, soldAmount).
-    """
+def addProductToGroup(productGroupId, pkProduct, productName, productDescription, productImagePath, productPrice, stock):
     client = getMongoConnection()
-    db = client.get_default_database()
-    collectionProducts = db['Products']
-    
-    # Set default fields for the product
-    productData['createDate'] = datetime.now()
-    productData['updateDate'] = datetime.now()
-    productData['isDelete'] = False
-    
-    result = collectionProducts.update_one(
-        { "_id": ObjectId(productGroupId) },
-        { "$push": { "product": productData },
-          "$set": { "updateDate": datetime.now() } }
-    )
-    print(f"Product added to product group {productGroupId}. Modified count: {result.modified_count}")
-    return result
-
-# Example usage:
-if __name__ == '__main__':
-    sampleProduct = {
-        "pkProduct": 101,
-        "productName": "New Product",
-        "productDescription": "Description of new product",
-        "productImagePath": ["path/to/image1.jpg", "path/to/image2.jpg"],
-        "productPrice": 99.99,
-        "stock": 50,
-        "soldAmount": 0
+    db = client['marketsync']
+    collection = db['Products']
+    newProduct = {
+        "pkProduct": pkProduct,
+        "productName": productName,
+        "productDescription": productDescription,
+        "productImagePath": productImagePath,  # expects a list of image paths
+        "productPrice": productPrice,
+        "stock": stock,
+        "soldAmount": 0,
+        "createDate": datetime.now(),
+        "updateDate": datetime.now(),
+        "isDelete": False
     }
-    createProductInGroup("603d2c1e3b1a4f2a6c8a9d7e", sampleProduct)
+    result = collection.update_one({"_id": ObjectId(productGroupId)}, {"$push": {"product": newProduct}})
+    print("Product added to group. Modified count:", result.modified_count)
+    return result.modified_count
+
+if __name__ == "__main__":
+    addProductToGroup("60d5f4832f8fb814b56fa181", 101, "New Product", "Product description", ["path/to/product.jpg"], 99.99, 10)
