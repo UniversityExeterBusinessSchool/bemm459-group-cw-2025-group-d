f87059320ef477d9d76ebf9b1fd12ab7228886d6
Thanit Wongmasa <tw707@exeter.ac.uk>
2025-03-18 19:04:36 +0000

    delete file

--- Docker/docker_run.sh
+++ Docker/docker_run.sh
@@ -3,6 +3,3 @@ docker run -d --name marketsync_mssql_server -p 10001:1433 -e "ACCEPT_EULA=Y" -e
 
 # Mongodb
 docker run -d --name marketsync_mongodb -p 10003:27017 -e MONGO_INITDB_ROOT_USERNAME=system_admin -e MONGO_INITDB_ROOT_PASSWORD=marketsyncpassword mongo:8.0
-
-# Elastic Search
-docker run -d --name marketsync_elasticsearch
--- Python/Library/DatabaseConnection.py
+++ Python/Library/DatabaseConnection.py
@@ -3,7 +3,6 @@ import os
 # Add the parent directory (Project) to sys.path
 # ENV file
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 # MSSQL connection
 import pyodbc
 # MongoDB connection
@@ -11,7 +10,7 @@ from pymongo import MongoClient
 from pymongo.database import Database as MongoDatabase
 # import env file
 import env
-from typing import Literal, Callable, Any
+from typing import Literal
 
 def queryMSSQL(operation: Literal["SELECT", "INSERT", "UPDATE", "DELETE"], query: str, params: tuple = ()):
     mssql_connection = None
@@ -76,73 +75,3 @@ def getMongoConnection() -> MongoDatabase:
     )
     client = mongo_connection[env.mongoDB_database]
     return client
-
-# # PostgreSQL connection
-# import psycopg2
-# PostgreSQL connection
-# def queryPostgreSQL(operation: Literal["SELECT", "INSERT", "UPDATE", "DELETE"], query: str):
-#     try:
-#         postgres_connection = psycopg2.connect(
-#             host=env.postgreSQL_host,
-#             port=env.postgreSQL_port,
-#             database=env.postgreSQL_database,
-#             user=env.postgreSQL_user,
-#             password=env.postgreSQL_password
-#         )
-#         cursor = postgres_connection.cursor()
-#         if operation == "SELECT":
-#             # Executing the SQL query
-#             cursor.execute(query)
-#             # Fetching all rows from the executed query
-#             records = cursor.fetchall()
-#             return records
-#         elif operation == "INSERT":
-#             # Execute insert query
-#             cursor.execute(query)
-#             # Fetch inserted data
-#             row = cursor.fetchone()
-#             # Commit the transaction
-#             postgres_connection.commit()
-#             return row
-#         elif operation == "UPDATE":
-#             # Execute update query
-#             cursor.execute(query)
-#             # Commit the transaction
-#             postgres_connection.commit()
-#         elif operation == "DELETE":
-#             # Execute delete query
-#             cursor.execute(query)
-#             # Commit the transaction
-#             postgres_connection.commit()
-#         else:
-#             raise ValueError("Invalid operation type")
-            
-#     except Exception as error:
-#         print("PostgreSQL Error:" + str(error))
-#     finally:
-#         # Closing the database connection
-#         if postgres_connection:
-#             cursor.close()
-#             postgres_connection.close()
-#             print("PostgreSQL connection is closed")
-
-# def queryFunctionPostgreSQL(functionQuery,functionParameter):
-#     try:
-#         postgres_connection = psycopg2.connect(
-#             host=env.postgreSQL_host,
-#             port=env.postgreSQL_port,
-#             database=env.postgreSQL_database,
-#             user=env.postgreSQL_user,
-#             password=env.postgreSQL_password
-#         )
-#         cursor = postgres_connection.cursor()
-#         result = functionQuery(cursor = cursor,postgres_connection = postgres_connection,functionParameter = functionParameter)
-#         return result
-#     except Exception as error:
-#         print("PostgreSQL Error:" + str(error))
-#     finally:
-#         # Closing the database connection
-#         if postgres_connection:
-#             cursor.close()
-#             postgres_connection.close()
-#             print("PostgreSQL connection is closed")
